a873a265bd49323fc8f6e693d194f4b89817fa6b
lossatsea <1315404687@qq.com>
2019-06-07 01:57:43 +0800

    ShaoZiShuo20190607

---
+++ src/variables/serverFunc.jsx
@@ -0,0 +1,35 @@
+export function handleResponse(response) {
+    return response.text().then(text => {
+        console.log(text);
+        const data = text && JSON.parse(text);
+        if (!response.ok) {
+            if (response.status === 401) {
+                // auto logout if 401 response returned from api
+                //logout();
+                //location.reload(true);
+            }
+
+            const error = (data && data.message) || response.statusText;
+            return Promise.reject(error);
+        }
+        return JSON.parse(text);
+    });
+}
+
+
+export function parseParams(data) {
+    try {
+        var tempArr = [];
+        for (var i in data) {
+            var key = encodeURIComponent(i);
+            var value = encodeURIComponent(data[i]);
+            tempArr.push(key + '=' + value);
+        }
+        var urlParamsStr = tempArr.join('&');
+        return urlParamsStr;
+    } catch (err) {
+        return '';
+    }
+}
+
+export const apiUrl = 'https://littlefish33.cn:8080/user';
--- src/views/LoginPage/LoginPage.jsx
+++ src/views/LoginPage/LoginPage.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import GridContainer from "components/Grid/GridContainer.jsx";
-import GridItem from "components/Grid/GridItem.jsx";
 import TextField from '@material-ui/core/TextField';
 import Button from "components/CustomButtons/Button.jsx";
-import { userActions } from '../_actions';
+import {handleResponse, parseParams, apiUrl} from "variables/serverFunc.jsx"
 
 class LoginPage extends React.Component {
     constructor(props) {
@@ -17,14 +16,46 @@ class LoginPage extends React.Component {
             userphone: null,
             password: null,
             submitted: false,
+            responseMsg:" ",
+            user_error:false,
+            password_error:false
         };
 
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+        this.login = this.login.bind(this);
     }
     handleChange(e) {
         const { name, value } = e.target;
         this.setState({ [name]: value });
+        this.setState({responseMsg: ""});
+    }
+
+    login(userphone, password){
+        const requestOptions = {
+            method: 'POST',
+            headers: { 
+                'Accept': 'application/json',
+                'content-type': 'application/x-www-form-urlencoded'
+            },
+            body: parseParams({"phone":userphone, "password":password})
+        };
+        fetch(apiUrl+"/login",requestOptions)
+        .then(handleResponse)
+        .then(response => {
+            // store user details and jwt token in local storage to keep user logged in between page refreshes
+            if(response.code === 200){
+                localStorage.setItem('user-token', response.data.token);
+                this.props.history.push('/');
+            }else{
+                const msg = response.msg;
+                this.setState({responseMsg: msg});
+                if(response.code === 400)
+                    this.setState({user_error: true, password_error: false});
+                else if(response.code === 401)
+                    this.setState({user_error: false, password_error: true});
+            }
+        });
     }
 
     handleSubmit(e) {
@@ -34,23 +65,23 @@ class LoginPage extends React.Component {
         const { userphone, password } = this.state;
         const { dispatch } = this.props;
         if (userphone && password) {
-            userActions.login(userphone, password);
+            this.login(userphone, password);
         }
     }
 
     render() {
         const { loggingIn } = this.props;
-        const { userphone, password, submitted } = this.state;
+        const { userphone, password, submitted, responseMsg, user_error, password_error } = this.state;
         return (
 		<div>
 			<GridContainer justify="center" direction="column" alignItems="center">
             <div className="col-md-6 col-md-offset-3" style={{width:"400px"}}>
-				<h2>Login</h2>
+				<h2>登录</h2>
                 <form name="form" onSubmit={this.handleSubmit}>
                     <div className={'form-group' + (submitted && !userphone ? ' has-error' : '')}>
 						<TextField
 							id="phone"
-							label="Phone"
+							label="手机号"
 							type="text"
 							className="form-control"
 							value={userphone}
@@ -62,13 +93,16 @@ class LoginPage extends React.Component {
 							fullWidth
 						  />
                         {submitted && !userphone &&
-                            <div className="help-block">Phone is required</div>
+                            <div className="help-block" style={{color: "red"}}>手机号不可为空</div>
+                        }
+                        {submitted && userphone && user_error &&
+                            <div className="help-block" style={{color: "red"}}>{responseMsg}</div>
                         }
                     </div>
                     <div className={'form-group' + (submitted && !password ? ' has-error' : '')}>
                         <TextField
 							id="password"
-							label="Password"
+							label="密码"
 							className="form-control"
 							type="password"
 							value={password}
@@ -81,16 +115,19 @@ class LoginPage extends React.Component {
 							fullWidth
 						  />
 						  {submitted && !password &&
-                            <div className="help-block">Password is required</div>
+                            <div className="help-block" style={{color: "red"}}>密码不可为空</div>
+                          }
+                          {submitted && password && !user_error && password_error &&
+                            <div className="help-block" style={{color: "red"}}>{responseMsg}</div>
 						  }
                     </div>
                     <div className="form-group">
-                        <Button className="btn btn-primary" color="primary" type="submit">Login</Button>
+                        <Button className="btn btn-primary" color="primary" type="submit">登录</Button>
                         {loggingIn &&
                             <img src="data:image/gif;base64,R0lGODlhEAAQAPIAAP///wAAAMLCwkJCQgAAAGJiYoKCgpKSkiH/C05FVFNDQVBFMi4wAwEAAAAh/hpDcmVhdGVkIHdpdGggYWpheGxvYWQuaW5mbwAh+QQJCgAAACwAAAAAEAAQAAADMwi63P4wyklrE2MIOggZnAdOmGYJRbExwroUmcG2LmDEwnHQLVsYOd2mBzkYDAdKa+dIAAAh+QQJCgAAACwAAAAAEAAQAAADNAi63P5OjCEgG4QMu7DmikRxQlFUYDEZIGBMRVsaqHwctXXf7WEYB4Ag1xjihkMZsiUkKhIAIfkECQoAAAAsAAAAABAAEAAAAzYIujIjK8pByJDMlFYvBoVjHA70GU7xSUJhmKtwHPAKzLO9HMaoKwJZ7Rf8AYPDDzKpZBqfvwQAIfkECQoAAAAsAAAAABAAEAAAAzMIumIlK8oyhpHsnFZfhYumCYUhDAQxRIdhHBGqRoKw0R8DYlJd8z0fMDgsGo/IpHI5TAAAIfkECQoAAAAsAAAAABAAEAAAAzIIunInK0rnZBTwGPNMgQwmdsNgXGJUlIWEuR5oWUIpz8pAEAMe6TwfwyYsGo/IpFKSAAAh+QQJCgAAACwAAAAAEAAQAAADMwi6IMKQORfjdOe82p4wGccc4CEuQradylesojEMBgsUc2G7sDX3lQGBMLAJibufbSlKAAAh+QQJCgAAACwAAAAAEAAQAAADMgi63P7wCRHZnFVdmgHu2nFwlWCI3WGc3TSWhUFGxTAUkGCbtgENBMJAEJsxgMLWzpEAACH5BAkKAAAALAAAAAAQABAAAAMyCLrc/jDKSatlQtScKdceCAjDII7HcQ4EMTCpyrCuUBjCYRgHVtqlAiB1YhiCnlsRkAAAOwAAAAAAAAAAAA==" />
                         }
                         <Link to="/register" >
-							<Button color="primary">Register</Button>
+							<Button color="primary">注册</Button>
 						</Link>
                     </div>
                 </form>
